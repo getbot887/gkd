40a829310c7b51345565538c32fa074755fb6e03
getbot887 <123931336+getbot887@users.noreply.github.com>
2024-08-29 15:27:14 +0800

    Update GetWANIP.sh

--- GetWANIP.sh
+++ GetWANIP.sh
@@ -6,9 +6,10 @@ export PATH=/usr/bin:/bin
 # 获取当前时间
 current_time=$(date "+%Y-%m-%d %H:%M:%S")
 
-# 使用curl获取IP信息
+CURL_CMD="/data/data/com.termux/files/usr/bin/curl"
 
-ipp_info=$(curl -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36' myip.ipip.net)
+# 使用curl获取IP信息
+ipp_info=$($CURL_CMD -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36' myip.ipip.net)
 
 echo "$current_time - $ipp_info" >> ./ipp.txt
 
